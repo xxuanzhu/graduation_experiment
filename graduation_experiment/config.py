30b2c4b218edf400b48edc8e780a532e0fcbc7cf
xxuanzhu <434966069@qq.com>
2022-04-23 11:51:17 +0800

    2022-4-23 暂改代码

---
+++ graduation_experiment/config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：graduation_experiment 
+@File    ：config.py
+@Author  ：xxuanZhu
+@Date    ：2022/4/23 11:43 
+@Purpose :
+'''
+
+from env import Env
+from agent import Agent
+
+
+DIC_AGENTS = {
+    "myAgent": Agent
+
+}
+
+
+DIC_ENVS = {
+    "env" : Env
+}
